c2d1488c87c19f5002204e9c563b147b2ee9b8f8
Tom <tomiam8@outlook.com>
2018-11-03 20:45:58 +1100

    Update time & message for 2019 FRC kickoff

--- script.js
+++ script.js
@@ -655,7 +655,7 @@ function initial() {
 }
 
 function updateClock() {
-	time = 1519189200-Math.round(Date.now() / 1000);
+	time = 1546698600-Math.round(Date.now() / 1000);
 	document.getElementById("days").innerHTML = Math.floor(time/86400);
 	document.getElementById("hours").innerHTML = Math.floor((time/3600)%24);
 	document.getElementById("minutes").innerHTML = Math.floor((time/60)%60);
